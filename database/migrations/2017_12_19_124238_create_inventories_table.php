8e665b8735c558cb9cb324c340ac4f9701aa53d3
Prabhat Kumar <prabhat2k15@gmail.com>
2017-12-21 15:36:47 +0530

    update

---
+++ database/migrations/2017_12_19_124238_create_inventories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('subid');
+            $table->integer('totalqty');
+            $table->integer('qty');//available quantity
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
